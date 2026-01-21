5de19546a8f0030a6d07fa512e7c6343b62013c8
UW-Example-Student <chris.holstrom@gmail.com>
2026-01-20 17:15:15 -0800

    Update vinyl.R
    New version makes the "missing vinyl" feature interactive. Also removes the count per year.

--- vinyl.R
+++ vinyl.R
@@ -1,18 +1,6 @@
-library(dplyr)
-library(DT)
-
-# Grab the music data.
-# See documentation for how to format the data
-album_data <- read.csv("data/album-rankings.csv")
-
 # Find top-rated albums that the person does not own on vinyl
-missing_vinyl <- function(){
-  buy_vinyl <- select(filter(album_data[order(-album_data$Rating),], Rating >= 9, Vinyl ==""), Album, Artist, Rating)
+missing_vinyl <- function(missing_vinyl_rating.var){
+  buy_vinyl <- select(filter(album_data[order(-album_data$Rating),], Rating >= as.numeric(missing_vinyl_rating.var), Vinyl ==""), Album, Artist, Rating)
 }
+missing_vinyl(10)
 
-year_most_vinyl <-function(){
-  most_vinyl <- select(filter(album_data, Vinyl == "v"), Year)
-  most_vinyl_count <- filter(most_vinyl) %>% count(Year)
-  most_vinyl_sort <- most_vinyl_count[order(-most_vinyl_count$n),] %>%
-  rename(Year = Year, Number = n)
-}
