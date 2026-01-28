4dbeb21157ba64ea0c6edd65955bfb1058876f45
UW-Example-Student <chris.holstrom@gmail.com>
2026-01-27 22:16:43 -0800

    Reorder the tabs

--- app_server.R
+++ app_server.R
@@ -30,7 +30,24 @@ server <- function(input, output) {
     return(number_one_album(input$rng[1], input$rng[2]))
   })
 
-# Second tab - Band and Artist Summary
+# Second tab - Top Albums By Year
+  output$text4 <- renderUI({
+    HTML("<h2>Top Albums by Year</h2><br>")
+  })
+
+  output$text5 <- renderUI({
+    HTML("<br><br>")
+  })
+
+  observeEvent(input$action_button2,{
+
+    output$year_table <- renderTable({
+      return(year_albums(input$year))
+    })
+
+  })
+
+  # Third tab - Band and Artist Summary
   output$text <- renderUI({
     HTML("<h2>About an Artist's Albums</h2><br>")
   })
@@ -53,35 +70,15 @@ server <- function(input, output) {
     })
 
   })
-
-  # Third tab - Top Albums By Year
-  output$text4 <- renderUI({
-    HTML("<h2>Top Albums by Year</h2><br>")
+  # Fourth tab - Favorite Artists
+  output$text10 <- renderUI({
+    HTML("<h2>Artists with Highly Rated Albums</h2><br>")
   })
-
-  output$text5 <- renderUI({
-    HTML("<br><br>")
-  })
-
-  observeEvent(input$action_button2,{
-
-    output$year_table <- renderTable({
-      return(year_albums(input$year))
+  observeEvent(input$action_button4,{
+    output$fav_artists_table <- renderTable({
+      return(favorite_bands(input$min_albums, input$live_ep_checkbox))
     })
-
-  })
-
-  # Fourth tab - Vinyl
-  output$text6 <- renderUI({
-    HTML("<h2>Top-Rated Albums Not Owned on Vinyl</h2><br>")
-  })
-
-  observeEvent(input$action_button3,{
-  output$missing_vinyl_table <- renderTable({
-    return(missing_vinyl(input$vinyl_rating))
-  })
-  })
-
+})
   # Fifth tab - Comparison Graph
   output$text8 <- renderUI({
     HTML("<h2>Compare Two Artists on the Graph</h2><br>")
@@ -95,15 +92,15 @@ server <- function(input, output) {
     return(band_album_comparison_chart(input$band_name_1, input$band_name_2))
   })
 
-
-
-  # Sixth tab - Favorite Artists
-  output$text10 <- renderUI({
-    HTML("<h2>Artists with Highly Rated Albums</h2><br>")
+  # Sixth tab - Vinyl
+  output$text6 <- renderUI({
+    HTML("<h2>Top-Rated Albums Not Owned on Vinyl</h2><br>")
   })
-  observeEvent(input$action_button4,{
-  output$fav_artists_table <- renderTable({
-    return(favorite_bands(input$min_albums, input$live_ep_checkbox))
-  })
+
+  observeEvent(input$action_button3,{
+    output$missing_vinyl_table <- renderTable({
+      return(missing_vinyl(input$vinyl_rating))
+    })
+
   })
 }
