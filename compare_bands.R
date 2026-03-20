6e06ed61f15ea2f7e5a2babb000a877ca6295849
UW-Example-Student <chris.holstrom@gmail.com>
2026-03-20 12:37:02 -0700

    Update compare_bands.R

--- compare_bands.R
+++ compare_bands.R
@@ -10,11 +10,13 @@ band_album_comparison_chart <- function(var.artist1, var.artist2){
     geom_point(data = band2_albums, aes(x = Year, y = Rating, group = Album, color = Artist)) +
     xlab("Year")+
     ylab("Rating")+
-    theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +
+    theme(axis.text.x = element_text(angle = 90, vjust=0.5, hjust=1)) +
     labs(title = "Album Ratings")+
     scale_x_continuous(breaks=seq(earliest_year,most_recent_year,5))+
     scale_y_continuous(breaks=seq(0,10,1))+
-    expand_limits(x=c(earliest_year,most_recent_year), y=c(0,10))
-
+    expand_limits(x=c(earliest_year,most_recent_year), y=c(0,10)) +
+#    theme(legend.position='bottom')
+    theme(legend.position = c(0.1, 0.1))
   ggplotly(p1)
 }
+
