bf64127bb04c03cf9023bbdee7bac8bdcc363a90
UW-Example-Student <chris.holstrom@gmail.com>
2026-03-19 17:11:22 -0700

    Update app.R
    Two more libraries.

--- app.R
+++ app.R
@@ -3,6 +3,8 @@
 # Load libraries so they are available
 library(shiny)
 library(dplyr)
+library(ggplot2)
+library(plotly)
 
 # Use source() to execute the `app_ui.R` and `app_server.R` files. These will
 # define the UI value and server function respectively.
